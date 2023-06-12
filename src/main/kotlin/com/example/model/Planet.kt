64c6efcdb158d17d211c3bdddfc31f31bf647113
Behzod Halil <behzoddev@gmail.com>
2023-06-12 16:59:36 +0900

    feat: Add planet

---
+++ src/main/kotlin/com/example/model/Planet.kt
@@ -0,0 +1,10 @@
+package com.example.model
+
+data class Planet(
+    val id: String,
+    val name: String,
+    val description: String,
+    val createdTimestamp: Long,
+    val distanceFromSun: String
+)
+
