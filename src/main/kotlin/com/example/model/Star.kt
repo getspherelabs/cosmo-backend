4c1a679e81e037d017b3734f72a0a9c68014271f
Behzod Halil <behzoddev@gmail.com>
2023-06-12 16:59:05 +0900

    refactor: Add timestamp start and planet tables

---
+++ src/main/kotlin/com/example/model/Star.kt
@@ -0,0 +1,10 @@
+package com.example.model
+
+data class Star(
+    val id: String,
+    val name: String,
+    val description: String,
+    val size: String,
+    val createdTimestamp: Long,
+    val distanceFromSun: String
+)
