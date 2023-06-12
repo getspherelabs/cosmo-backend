ba2fa6b70c1b6d8386adb3f8ae1cdb41585aa544
Behzod Halil <behzoddev@gmail.com>
2023-06-12 16:59:53 +0900

    refactor: Add timestamp to planet table

--- src/main/kotlin/com/example/table/PlanetTable.kt
+++ src/main/kotlin/com/example/table/PlanetTable.kt
@@ -1,10 +1,13 @@
 package com.example.table
 
 import org.jetbrains.exposed.dao.id.UUIDTable
+import org.jetbrains.exposed.sql.jodatime.datetime
+import org.joda.time.DateTime
 
-object Planet : UUIDTable() {
+object PlanetTable : UUIDTable(name = "Planet") {
     var name = varchar(name = "name", length = 30)
     var size = text(name = "size")
     var distanceFromSun = text(name = "distance_from_sun")
     var description = text(name = "description")
+    var createdTimestamp = datetime("created_timestamp").default(DateTime.now())
 }
