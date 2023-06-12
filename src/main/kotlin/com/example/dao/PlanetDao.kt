a72b90501286aad323080f02c6a65f199112a45d
Behzod Halil <behzoddev@gmail.com>
2023-06-12 17:00:10 +0900

    feat: Add planet dao

---
+++ src/main/kotlin/com/example/dao/PlanetDao.kt
@@ -0,0 +1,37 @@
+package com.example.dao
+
+import com.example.common.dbQuery
+import com.example.entity.PlanetEntity
+import com.example.entity.asPlanet
+import com.example.model.Planet
+import java.util.*
+
+interface PlanetDao {
+    suspend fun insert(name: String, description: String, size: String, distanceFromSun: String): String
+    suspend fun getPlanets(): List<Planet>
+    suspend fun removeById(id: String): Boolean
+}
+
+class DefaultPlanetDao : PlanetDao {
+    override suspend fun insert(name: String, description: String, size: String, distanceFromSun: String): String = dbQuery {
+        PlanetEntity.new {
+            this.name = name
+            this.description = description
+            this.size = size
+            this.distanceFromSun = distanceFromSun
+        }.id.value.toString()
+    }
+
+    override suspend fun getPlanets(): List<Planet> = dbQuery {
+        PlanetEntity.all().sortedBy { it.id }.map { it.asPlanet() }
+    }
+
+    override suspend fun removeById(id: String): Boolean = dbQuery {
+        val currentPlant = PlanetEntity.findById(UUID.fromString(id))
+        currentPlant?.run {
+            delete()
+            return@dbQuery true
+        }
+        return@dbQuery false
+    }
+}
