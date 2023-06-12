2370b2f1a46cdd312b22573073c4634210189579
Behzod Halil <behzoddev@gmail.com>
2023-06-12 16:58:39 +0900

    build: Add hikari and koin dependencies

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,6 +1,8 @@
 object Version {
     const val exposed = "0.40.1"
     const val postgresql = "42.5.4"
+    const val hikari = "2.3.13"
+    const val koin = "2.0.1"
 }
 
 object Libs {
@@ -8,9 +10,18 @@ object Libs {
         const val core = "org.jetbrains.exposed:exposed-core:${Version.exposed}"
         const val dao = "org.jetbrains.exposed:exposed-dao:${Version.exposed}"
         const val jdbc = "org.jetbrains.exposed:exposed-jdbc:${Version.exposed}"
+        const val jodatime = "org.jetbrains.exposed:exposed-jodatime:${Version.exposed}"
     }
 
     object Postgresql {
         const val core = "org.postgresql:postgresql:${Version.postgresql}"
+    }
+
+    object Hikari {
+        const val core = "com.zaxxer:HikariCP:${Version.hikari}"
+    }
+
+    object Koin {
+        const val ktor = "org.koin:koin-ktor:${Version.koin}"
     }
 }
