1b38e0983c93ecab761932d5bc6efd5f7cf8090a
Behzod Halil <behzoddev@gmail.com>
2023-07-10 17:58:23 +0900

    feat: Add http response

---
+++ src/main/kotlin/com/example/model/HttpResponse.kt
@@ -0,0 +1,34 @@
+package com.example.model
+
+import io.ktor.http.HttpStatusCode
+
+sealed class HttpResponse<TYPE : Response> {
+    abstract val body: TYPE
+    abstract val code: HttpStatusCode
+
+    data class Success<TYPE : Response>(override val body: TYPE) : HttpResponse<TYPE>() {
+        override val code: HttpStatusCode = HttpStatusCode.OK
+    }
+
+    data class NotFound<TYPE : Response>(override val body: TYPE) : HttpResponse<TYPE>() {
+        override val code: HttpStatusCode = HttpStatusCode.NotFound
+    }
+
+    data class BadRequest<TYPE : Response>(override val body: TYPE) : HttpResponse<TYPE>() {
+        override val code: HttpStatusCode = HttpStatusCode.BadRequest
+    }
+
+    companion object {
+        fun <TYPE : Response> success(response: TYPE) = Success(body = response)
+        fun <TYPE : Response> notFound(response: TYPE) = NotFound(body = response)
+        fun <TYPE : Response> badRequest(response: TYPE) = BadRequest(body = response)
+    }
+}
+
+fun httpResponse(response: Response): HttpResponse<Response> {
+    return when(response.status) {
+        Status.Failed -> HttpResponse.badRequest(response)
+        Status.NotFound -> HttpResponse.notFound(response)
+        Status.Success -> HttpResponse.success(response)
+    }
+}
