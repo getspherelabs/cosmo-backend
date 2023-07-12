fb46dcebaffe4eb805f63dc54549d65c2cfedbf6
Behzod Halil <behzoddev@gmail.com>
2023-07-12 17:26:20 +0900

    refactor: Update module function

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,16 +1,9 @@
 package com.example
 
-
-import com.example.config.DatabaseFactory
-import com.example.di.controllerModule
-import com.example.di.daoModule
-import com.example.di.testModule
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
-import org.koin.core.module.Module
-import org.koin.ktor.plugin.Koin
 
 
 fun main() {
@@ -19,18 +12,8 @@ fun main() {
 }
 
 fun Application.module() {
-    install(Koin) {
-        modules(listOf(controllerModule, daoModule, testModule))
-    }
+    configureKoin()
     configureSerialization()
     configureRouting()
-}
-
-fun testKoinModule(module: Module): Application.() -> Unit {
-    fun Application.testModule() {
-        install(Koin) {
-            modules(module)
-        }
-    }
-    return Application::testModule
+    configureCors()
 }
