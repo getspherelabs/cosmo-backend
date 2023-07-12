638bfa4f5a9b06ed4539d49127ddaa1a6f7b3231
Behzod Halil <behzoddev@gmail.com>
2023-07-12 17:24:54 +0900

    feat: Add configuration cors

---
+++ src/main/kotlin/com/example/plugins/ConfigureCors.kt
@@ -0,0 +1,16 @@
+package com.example.plugins
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.*
+
+fun Application.configureCors() {
+    install(CORS) {
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowCredentials = true
+        anyHost()
+    }
+}
