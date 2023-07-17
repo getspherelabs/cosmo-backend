02036732df4724aa7ddc6b87b4254d662e4355f1
behzoddev <behzoddev@gmail.com>
2023-07-18 06:23:23 +0900

    build: Update build gradle settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,6 @@ version = "0.0.1"
 
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
-
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
