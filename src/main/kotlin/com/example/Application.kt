796c62fa9d23db0eb99b9a22a0db8fa110c96fb7
Behzod Halil <behzoddev@gmail.com>
2023-07-18 16:36:53 +0900

    chore: Update application conf

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -7,13 +7,10 @@ import com.example.common.plugins.configureSerialization
 import com.example.common.plugins.configureSwagger
 import io.ktor.server.application.*
 import com.example.plugins.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import io.ktor.util.*
 
-fun main(args: Array<String>) {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
 
 fun Application.module() {
     configureKoin()
