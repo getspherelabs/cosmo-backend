0b4f4b340f0c088dd2eb0a112838d81aceb56b9c
Behzod Halil <behzoddev@gmail.com>
2023-07-18 16:25:19 +0900

    chore: Update dockerfile #10

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -7,9 +7,13 @@ import com.example.common.plugins.configureSerialization
 import com.example.common.plugins.configureSwagger
 import io.ktor.server.application.*
 import com.example.plugins.*
-import io.ktor.util.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>) {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
 
 fun Application.module() {
     configureKoin()
